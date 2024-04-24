58528379e95d34c0bb6d6bf18126e232e461efed
Thakur <mrhimachal1903@gmail.com>
2024-04-25 00:40:52 +0530

    JWT TOKEN GENERATION

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -42,4 +42,32 @@ require:true}
   
 
 },{timestamps:true})
+userSchema.pre("save",async function(next){//avoid using arrow function because they do not have context hence can't use 'this'
+  if(this.isModified("password")){
+    this.password=bcrypt.hash(this.password,10);//agar password change kia hai tbhi bcrypt krenge
+  }
+next()
+})
+userSchema.methods.isPasswordCorrect=async function(password){
+  return await bcrypt.compare(password,this.password);//check if password entered is correct or not
+}
+userSchema.methods.generateAccessToken=async function(){
+  return jwt.sign({//check kro async await lagega ya naho
+  _id:this._id,
+  email:this.email,
+  username:this.username,
+  fullName:this.fullName, 
+  },process.env.ACCESS_TOKEN_SECRET,{
+    expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+  })
+}
+userSchema.methods.generateRefreshToken=async function(){
+  return jwt.sign({//check kro async await lagega ya naho
+    _id:this._id,
+                                     
+    },process.env.REFRESH_TOKEN_SECRET,{
+      expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    })
+}
+
 export const User=mongoose.model('User',userSchema);
